9d10683fb27976e3caec4b9e38cdbd1d6cc4518b
licasterian <lica.sterian@gmail.com>
2017-04-13 14:19:03 +0300

    removed test struct json tags

--- csvDecoder_test.go
+++ csvDecoder_test.go
@@ -7,9 +7,9 @@ import (
 )
 
 type Entry struct {
-	Id        int `csv:"id" json:"id"`
-	Nickname  string `csv:"nickname" json:"nickname"`
-	CreatedOn time.Time `csv:"createdOn" csvDate:"2006-01-02" json:"createdOn"`
+	Id        int `csv:"id"`
+	Nickname  string `csv:"nickname"`
+	CreatedOn time.Time `csv:"createdOn" csvDate:"2006-01-02"`
 }
 
 func TestDecode(t *testing.T) {
@@ -17,7 +17,7 @@ func TestDecode(t *testing.T) {
 0,SLIde,2017-04-12`
 	buf := bytes.NewBufferString(content)
 	decoder := NewDecoder(buf)
-	var rows []Entry
+	var rows []*Entry
 	err := decoder.Decode(&rows)
 	if err != nil {
 		t.Fatalf("decode error: %s", err.Error())
@@ -29,6 +29,5 @@ func TestDecode(t *testing.T) {
 	if firstEntry.Id != 0 || firstEntry.Nickname != "SLIde" || firstEntry.CreatedOn.Format("2006-01-02") != "2017-04-12" {
 		t.Fatalf(`First csv entry should be {Id:0, Nickname:SLIde, CreatedOn: 2017-04-12 00:00:00 +0000UTC}, but it's %+v`, firstEntry)
 	}
-	rows[0].Id = 3
 	t.Logf("%#v", firstEntry)
 }
